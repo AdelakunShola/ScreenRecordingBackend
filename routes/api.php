6ed441226b84f2165f927eb663ca17de1ff4b941
Adelakun Shola <officialadelakun@gmail.com>
2023-10-01 03:17:43 -1100

    api

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,18 @@ use App\Http\Controllers\VideoController;
 
 
 
-Route::post('/videos', [VideoController::class, 'store']);
-Route::get('/videos', [VideoController::class, 'index']);
-Route::get('/videos/{id}', [VideoController::class, 'show']);
-Route::delete('/videos/{id}', [VideoController::class, 'destroy']);
+Route::prefix('')->group(function () {
+ 
+
+    // Store (Create a new person)
+    Route::post('/videos', [VideoController::class, 'store']);
+
+        // Index (List all persons)
+        Route::get('/', [App\Http\Controllers\VideoController::class, 'index']);
+
+      // Show (Get a specific person by ID)
+      Route::get('/videos/{id}', [VideoController::class, 'show']);
+
+    // Delete (Delete a person by ID)
+    Route::delete('/videos/{id}', [VideoController::class, 'destroy']);
+});
