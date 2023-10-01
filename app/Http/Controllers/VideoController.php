01ba1c7ceb748d57f5dd67b8f0ddb6ac5c35cf7c
Adelakun Shola <officialadelakun@gmail.com>
2023-10-01 01:32:04 -1100

    controller update

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,47 +4,84 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Video;
-use Illuminate\Support\Facades\Storage;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Support\Facades\File;
 use getID3;
 use GuzzleHttp\Client;
-use DateTime;
 
 class VideoController extends Controller
 {
     public function store(Request $request)
     {
-         // Validate the request
-         $request->validate([
-           
+        // Validate the request
+        $request->validate([
+            'title' => 'nullable|string',
             'file' => 'required|file|mimes:mp4,mov,avi',
             'thumbnail' => 'nullable|image',
-           
+            'description' => 'nullable|string',
         ]);
 
-        // Handle file upload to local storage
+        // Handle file upload to Cloudinary
         $file = $request->file('file');
         $fileName = $file->getClientOriginalName();
         $fileSize = $file->getSize();
 
-        // Upload video to local storage (assuming 'videos' is the disk name)
-        $path = $file->storeAs('videos', $fileName, 'videos');  // Use storeAs to set a specific filename
-        $fileLink = asset(Storage::url($path));
+         // Open the file for reading
+         $handle = fopen($file->getRealPath(), 'rb');
 
-        // Handle optional thumbnail upload to local storage
+        // Determine chunk size (adjust as needed)
+        $chunkSize = 1024 * 1024; // 1 MB chunks
+
+        // Initialize Cloudinary public ID and file link
+        $publicId = null;
+        $fileLink = null;
+
+        // Initialize $transcription variable
+        $transcription = null;
+
+        try {
+             // Loop through chunks
+             while (!feof($handle)) {
+                // Read a chunk of data
+                $chunk = fread($handle, $chunkSize);
+
+                // Upload the chunk to Cloudinary
+                $uploadResult = Cloudinary::upload('data://text/plain;base64,' . base64_encode($chunk), [
+                    'folder' => 'videos',
+                    'resource_type' => 'video',
+                ]);
+
+                // Update Cloudinary public ID and file link
+                $publicId = $uploadResult->getPublicId();
+                $fileLink = $uploadResult->getSecurePath();
+             }
+
+         
+        } finally {
+            // Close the file handle
+            fclose($handle);
+        }
+
+        // Handle optional thumbnail upload to Cloudinary
         $thumbnailPath = null;
         if ($request->hasFile('thumbnail')) {
             $thumbnail = $request->file('thumbnail');
-            $thumbnailPath = $thumbnail->store('thumbnails', 'thumbnails');
+            $thumbnailUploadResult = Cloudinary::upload($thumbnail->getRealPath(), [
+                'folder' => 'thumbnails',
+                'resource_type' => 'image',
+            ]);
+            $thumbnailPath = $thumbnailUploadResult->getSecurePath();
         }
 
         // Create video record
         $video = Video::create([
-            'id' => Video::max('id') + 1,
-            'file_name' => $fileName,
-            'file_link' => asset(Storage::url($path)),
+            'title' => $request->input('title'),
+            'file_name' => $publicId, // Store Cloudinary public ID
+            'file_link' => $fileLink,
             'thumbnail' => $thumbnailPath,
+            'description' => $request->input('description'),
             'file_size' => $fileSize,
+            'transcription' => $transcription['transcription'] ?? null,  // Assuming the API response has a 'transcription' key
         ]);
 
         // Convert file size to megabytes
@@ -54,10 +91,9 @@ class VideoController extends Controller
         $video['file_size'] = $fileSizeMB . ' MB';
 
         // Get the duration of the video using getID3
-        $videoPath = storage_path('app/public/videos/' . $fileName);
-        $getID3 = new getID3();
+        $videoPath = public_path('videos/' . $publicId . '.mp4');
+        $getID3 = new \getID3();
         $fileInfo = $getID3->analyze($videoPath);
-        
 
         // Check if duration information is available
         if (isset($fileInfo['playtime_seconds'])) {
@@ -67,36 +103,66 @@ class VideoController extends Controller
             $formattedDuration = gmdate("H:i:s", $duration);
 
             // Update the 'length' field in the video
-            $video->length = $formattedDuration;
+            $video['length'] = $formattedDuration;
         } else {
             // Handle the case where duration information is not available
             $video['length'] = 'N/A';
         }
 
-        // Format the uploaded time in the desired format
-    $uploadedTime = new DateTime($video->uploaded_time);
-    $formattedUploadedTime = $uploadedTime->format('j, F Y');
+        // Assuming $fileLink contains the URL of the uploaded file on Cloudinary
+        $whisperApiUrl = 'https://transcribe.whisperapi.com';  // Replace with the actual WhisperAPI endpoint
+        $apiKey = 'N7G2M3UC7Z4JJYJY3VJ9RCEIVLQZ8UBT';  // Replace with the actual API key provided by WhisperAPI
 
-    $video->uploaded_time = $formattedUploadedTime;
+        // Make a request to WhisperAPI using Guzzle
+        $client = new Client();
+        $response = $client->post($whisperApiUrl, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $apiKey,
+            ],
+            'json' => [
+                'url' => $fileLink,
+            ],
+        ]);
 
+        // Check if the request was successful (status code 200)
+        if ($response->getStatusCode() === 200) {
+            $decodedResponse = json_decode($response->getBody(), true);
 
-        return response()->json([
-            'message' => 'Video uploaded successfully',
-            'video' => $video,
-        ], 201);
+            // Check if decoding was successful and the 'transcription' key exists
+            if ($decodedResponse !== null && isset($decodedResponse['transcription'])) {
+                $transcription = $decodedResponse['transcription'];
+            } else {
+                // Handle the case where the 'transcription' key is missing in the response
+                return response()->json([
+                    'error' => 'Transcription key missing in WhisperAPI response',
+                ], 500);
+            }
+
+            return response()->json([
+                'message' => 'Video uploaded and transcribed successfully',
+                'video' => $video,
+                'transcription' => $transcription,
+            ], 201);
+        } else {
+            // If the request was not successful, handle the error
+    $errorDetails = json_decode($response->getBody(), true);
+    $errorMessage = isset($errorDetails['error']) ? $errorDetails['error'] : 'Failed to transcribe the video';
+
+            
+    return response()->json([
+        'error' => $errorMessage,
+        'details' => $errorDetails,  // You can log or inspect the response body for details
+    ], 500);
+        }
     }
+
    
 
 
     public function index()
     {
         $videos = Video::all();
-
-        // For each video, retrieve the duration and include it in the response
-foreach ($videos as $video) {
-    // You can format the duration as needed
-    $video->duration = $video->length ?? 'N/A';
-}
         return response()->json($videos);
     }
 
